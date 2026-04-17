fix(model layer): proper gradient as backward input and output

src/mlp/layers/dense_layer.py
@@ -1,3 +1,7 @@
+"""
+    @TODO: (possibly) Add L2 regularization to the loss and gradients in backward().
+    @TODO: move the initializer logic to a separate method and call it from compile()
+"""
 import numpy as np
 from activations import Activation
 from initializers import WeightsInitializer
@@ -14,11 +18,17 @@ class DenseLayer:
     ===============================================================================================
 
     Attributes:
-        num_neurons (int): The number of neurons in the layer.
-        activation_function (Activation): The activation function to apply to the output of the layer.
-        weight_initializer (WeightsInitializer): The initializer for the weights of the layer.
-        weights (np.ndarray | None): The weights of the layer, shape (input_size, num_neurons).
-        biases (np.ndarray | None): The biases of the layer, shape (num_neurons,).
+        num_neurons - The number of neurons in the layer.
+        activation_function - The activation function to apply after the linear transformation.
+        weight_initializer - The initializer to use for the weights.
+        
+        weights - The weights of the layer 
+        biases - The biases of the layer 
+        grad_weights - Gradient of the loss with respect to weights 
+        grad_biases - Gradient of the loss with respect to biases 
+        
+        _z_cache - Cache for the linear transformation output before activation 
+        _input_cache - Cache for the input to the layer 
     """
 
     def __init__(
@@ -28,29 +38,34 @@ class DenseLayer:
         weight_initializer: WeightsInitializer,
     ):
         self.num_neurons = num_neurons
-
         self.activation_function = activation_function
+
         self.weight_initializer = weight_initializer
 
+        # weights and biases initialized in compile()
         self.weights: np.ndarray | None = None  # shape (input_size, num_neurons)
         self.biases: np.ndarray | None = None  # shape (num_neurons,)
+        self.grad_weights: np.ndarray | None = None  # shape (input_size, num_neurons)
+        self.grad_biases: np.ndarray | None = None  # shape (num_neurons,)
 
         # cache for backpropagation
         self._z_cache: np.ndarray | None = None  # shape (batch_size, num_neurons)
         self._input_cache: np.ndarray | None = None  # shape (batch_size, input_size)
 
-        # gradients populated by backward(), consumed by optimizer
-        self.grad_weights: np.ndarray | None = None  # shape (input_size, num_neurons)
-        self.grad_biases: np.ndarray | None = None  # shape (num_neurons,)
 
     def compile(self, input_size: int):
+        """Initializes the weights and biases of the layer based on the input size and the number 
+        of neurons.
 
+        Args:
+            input_size (int): number of features from the previous layer.
+        """
         self.weights = self.weight_initializer(input_size, self.num_neurons)
         self.biases = np.zeros(self.num_neurons)
 
     @requires_compiled
     def forward(self, inputs: np.ndarray) -> np.ndarray:
-        """Performs the forward pass through the layer.
+        """Forward pass through the layer.
 
         Args:
             inputs: shape(batch_size, input_size) The input data to the layer
@@ -63,25 +78,35 @@ class DenseLayer:
         self._input_cache = inputs
         self._z_cache = np.dot(inputs, self.weights) + self.biases
 
-        return self.activation_function.forward(self._z_cache)
+        return self.activation_function.activate(self._z_cache)
 
     @requires_compiled
     def backward(self, grad_output: np.ndarray) -> np.ndarray:
-        """Performs the backward pass through the layer.
+        """Backward pass through the layer.
 
         Args:
-            grad_output: shape (batch_size, num_neurons) — ∂L/∂output of this layer
+            grad_output - output gradient of the loss. shape (batch_size, num_neurons)
         Returns:
-            np.ndarray: shape (batch_size, input_size) — ∂L/∂input, passed to previous layer
+            np.ndarray - input gradient of the loss. shape (batch_size, input_size)
+        Exceptions:
+            ValueError: If the layer has not been compiled (weights and biases not initialized).
         """
-        grad_activation = grad_output * self.activation_function.backward(self._z_cache)
+        # gradient of the loss w.r.t. pre-activation output z
+        # ∂L/∂z_i = ∂L/∂a_i * ∂a_i/∂z_i
+        grad_activation = grad_output * self.activation_function.derivative(self._z_cache)
 
+        # Compute gradients of the loss w.r.t. weights and biases
+        # ∂L/∂w_i = ∂L/∂z_i * ∂z_i/∂w_i
         self.grad_weights = np.dot(
             self._input_cache.T, grad_activation
         )  # (input_size, num_neurons)
         self.grad_biases = np.sum(grad_activation, axis=0)  # (num_neurons,)
 
-        return np.dot(grad_activation, self.weights.T)  # (batch_size, input_size)
+        # compute gradient of the loss w.r.t. inputs to pass to previous layer
+        # ∂L/∂x_i = ∂L/∂z_i * ∂z_i/∂x_i
+        grad_input = np.dot(grad_activation, self.weights.T)  # (batch_size, input_size)
+        
+        return grad_input
 
     def is_compiled(self) -> bool:
         """Checks if the layer has been compiled (weights and biases initialized).